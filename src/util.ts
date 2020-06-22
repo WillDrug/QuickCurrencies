5e373056396159964d2f910cfe982c968c2044ce
Scott Harwood <scott.harwood@scottharwood.dev>
2020-06-22 20:16:11 +0100

    fix mentions for users without nicknames

--- src/util.ts
+++ src/util.ts
@@ -22,6 +22,6 @@ export const commandParser = (cmd: string): [string, string] => {
 }
 
 export const mentionHandler = (src: string): string[] => {
-  const matches = src.match(/<@!(\d+)>/g);
-  return matches ? matches.map((match) => match.substr(3).replace(">", "")): [];
+  const matches = src.match(/<@!?(\d+)>/g);
+  return matches ? matches.map((match) => match.substr(2).replace(">", "").replace("!","")): [];
 }; //return array of ids from string;
