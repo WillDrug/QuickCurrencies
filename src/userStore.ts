2a0abd3059b552549f09edd3c13f1221c0132f90
Scott Harwood <scott.harwood@scottharwood.dev>
2020-06-26 19:18:46 +0100

    fix: user without bucks no longer break

--- src/userStore.ts
+++ src/userStore.ts
@@ -8,50 +8,59 @@ export class UserStore {
   }
 
   public async addBucks(uid: string, amount: number, from: string, to: string) {
-    const currAmmount = (await db.collection(COLLECTION_NAME).doc(uid).get()).data();
+    const currAmmount = (
+      await db.collection(COLLECTION_NAME).doc(uid).get()
+    ).data();
     let endAmm;
     if (currAmmount) {
-      endAmm = Math.round((currAmmount.currency) + amount);
+      endAmm = Math.round(currAmmount.currency + amount);
     } else {
-      endAmm = amount 
+      endAmm = amount;
     }
-    await db.collection(COLLECTION_NAME).doc(uid).set({ currency: endAmm }, {merge: true});
+    await db
+      .collection(COLLECTION_NAME)
+      .doc(uid)
+      .set({ currency: endAmm }, { merge: true });
     logger.info(`${from} added ${amount} bucks to: ${to}`);
-
   }
 
   public async getTop(n: number): Promise<[string, number][]> {
-      const snapshot = await db.collection(COLLECTION_NAME).orderBy("currency", "desc").limit(n).get();
+    const snapshot = await db
+      .collection(COLLECTION_NAME)
+      .orderBy("currency", "desc")
+      .limit(n)
+      .get();
 
-   
-      const status:[string,number][] = [];
-        snapshot.forEach((sample) => {
-            const data = sample.data()
-            status.push([sample.id,data.currency])
-        })
+    const status: [string, number][] = [];
+    snapshot.forEach((sample) => {
+      const data = sample.data();
+      status.push([sample.id, data.currency]);
+    });
 
-      return status
+    return status;
   }
 
   public async getMyBalance(id: string): Promise<number> {
     const d = await db.collection(COLLECTION_NAME).doc(id).get();
-      const data = d.data() 
-      if (d.exists && data){
-        return data.currency
-      }
-      throw new Error("Invalid Id");
+    const data = d.data();
+    if (d.exists && data) {
+      return data.currency;
+    }
+    return 0;
   }
 
   public async makeItRain(amount: number, members: string[], from: string) {
     const bpp = amount / members.length;
-    await Promise.all(Array.from(members).map((id) => 
-      this.addBucks(
-        id,
-        Math.round(bpp + Math.ceil(bpp * 0.1 * Math.random())),
-        from,
-        id
+    await Promise.all(
+      Array.from(members).map((id) =>
+        this.addBucks(
+          id,
+          Math.round(bpp + Math.ceil(bpp * 0.1 * Math.random())),
+          from,
+          id
+        )
       )
-    ));
+    );
   }
 
   public async getPhoto(): Promise<{ location: string; text?: string }> {
@@ -64,7 +73,12 @@ export class UserStore {
     return photos[Math.floor(Math.random() * photos.length)];
   }
 
-  public async billAccount(id: string, amount: number, from: string, to: string) {
+  public async billAccount(
+    id: string,
+    amount: number,
+    from: string,
+    to: string
+  ) {
     if (amount < 0) {
       throw new Error("Tried to bill negative");
     }
